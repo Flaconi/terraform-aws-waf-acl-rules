a15feabcf3b774937b39afaa0c8085893b9927cd
maartenvanderhoef <maarten@vanderhoef.eu>
2019-08-05 10:48:34 +0200

    Clean-up

--- variables.tf
+++ variables.tf
@@ -1,31 +1,5 @@
-#{
-# 		name              = "name"
-# 		priority          = "1"
-# 		enabled           = false
-# 		byte_match_tuples = []
-# 		ranges = [
-# 			{
-# 				"value" = "127.0.0.1/32"
-# 			},
-# 		]
-# 		}, {
-# 		name              = "blockgoogle"
-# 		priority          = "2"
-# 		enabled           = true
-# 		byte_match_tuples = []
-# 		ranges = [
-# 			{
-# 				"value" = "8.8.4.4/32"
-# 			},
-# 			{
-# 				"value" = "8.8.8.8/32"
-# 			},
-# 		]
-# 	}]
-
 variable "waf_rules" {
   description = "Waf Rules"
-  #type        = any
   default = []
   type = list(object({
     name              = string
