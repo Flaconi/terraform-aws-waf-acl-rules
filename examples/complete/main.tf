c3073849574a5390846781d33f5553b6fa18fd16
Novikov Sergey <sergey.novikov@flaconi.de>
2020-03-11 17:58:18 +0100

    Add URI match (#2)
    * Add URI match
    
    * Fix terraform docs

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -3,39 +3,61 @@ provider "aws" {
 }
 
 locals {
-  waf_rules = [{
-    name              = "name"
-    priority          = "1"
-    enabled           = false
-    byte_match_tuples = []
-    ranges = [
-      {
-        "value" = "127.0.0.1/32"
-      },
-    ]
+  waf_rules = [
+    {
+      name              = "name"
+      priority          = "1"
+      enabled           = false
+      byte_match_tuples = []
+      uri_match = []
+      ranges = [
+        {
+          "value" = "127.0.0.1/32"
+        },
+      ]
     }, {
-    name              = "blockgoogle"
-    priority          = "2"
-    enabled           = true
-    byte_match_tuples = []
-    ranges = [
-      {
-        "value" = "8.8.4.4/32"
-      },
-      {
-        "value" = "8.8.8.8/32"
-      },
-    ]
-    }
-    , {
+      name              = "blockgoogle"
+      priority          = "2"
+      enabled           = true
+      byte_match_tuples = []
+      uri_match = []
+      ranges = [
+        {
+          "value" = "8.8.4.4/32"
+        },
+        {
+          "value" = "8.8.8.8/32"
+        },
+      ]
+    }, {
       name     = "allowheaderx"
       priority = "3"
       enabled  = true
       ranges   = []
-      byte_match_tuples = [{
-        field_to_match_data = "header-X",
-        target_string       = "containsthis"
-      }]
+      uri_match = []
+      byte_match_tuples = [
+        {
+          field_to_match_data = "header-X",
+          target_string       = "containsthis"
+        },
+      ]
+    }, {
+      name     = "allowPathWithToken"
+      priority = "4"
+      enabled  = true
+      ranges   = []
+      uri_match = [
+        {
+          target_string = "/your/uri/"
+        },
+      ]
+      byte_match_tuples = [
+        {
+          field_to_match_type = "QUERY_STRING"
+          field_to_match_data = null
+          target_string       = "token=SECRET"
+        },
+      ]
     }
   ]
 }
